4f36b123c8d312eec8f7766bb22fb57677930051
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-08-09 01:23:02 +0200

    Initial firefox release. Set version number to 1.1.3 to be in line with the chrome extension.

---
+++ README.md
@@ -0,0 +1,8 @@
+# shorts-dont-loop-firefox
+This is the official repository for "Shorts don't loop" – a Mozilla Firefox add-on that prevents YouTube Shorts from looping.
+
+The plugin can be easily activated and deactivated from the navigation bar – no need to go into the Mozilla Firefox add-on settings every time.
+
+[Download](https://github.com/nwawrzyniak/shorts-dont-loop-firefox/releases/latest)
+
+[Installation Guide](https://github.com/nwawrzyniak/shorts-dont-loop-firefox/wiki/How-to-install)
---
+++ content-scripts/sdl.js
@@ -0,0 +1,40 @@
+function preventLooping() {
+  console.info("sdl: DOM fully loaded and parsed. Beginning execution of script \"sdl.js\".");
+
+  browser.storage.sync.get("isActive").then(result => {
+    if (result.isActive === undefined) {
+      return browser.storage.sync.set({ isActive: true }).then(() => {
+        console.log("sdl: isActive was undefined, set to true by default.");
+      });
+    }
+  }).then(() => {
+    return browser.storage.sync.get("isActive");
+  }).then(result => {
+    if (result.isActive) {
+      let checkVideoInterval = setInterval(() => {
+        const url = window.location.href;
+        if (url.includes("/shorts/")) {
+          const video = document.querySelector("#shorts-player > div.html5-video-container > video");
+          if (video != null) {
+            console.log("sdl: video found.");
+            clearInterval(checkVideoInterval);
+            let loopCheckInterval = setInterval(() => {
+              if (video.loop) {
+                video.loop = false;
+                console.log("sdl: video.loop set to false.");
+              }
+            }, 5);
+          } else {
+            console.log("sdl: video not found, retrying...");
+          }
+        }
+      }, 1000);
+    }
+  });
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", preventLooping);
+} else {
+  preventLooping();
+}
